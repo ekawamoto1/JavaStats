60962d4e6af6aca7229b568cbf20acc90ab18fdf
ekawamoto1 <eric.kawamoto@baxter-academy.org>
2020-06-25 12:06:38 -0400

    modularize by making mean, stdev computations into user-defined functions

--- JavaStats.java
+++ JavaStats.java
@@ -7,12 +7,8 @@ public class JavaStats
     {
         ArrayList<Double> dArrL = new ArrayList<>();    // initialize empty ArrayList
         int n = 0;
-        double term = 0.0;
-        double sum = 0.0;
-        double mean = 0.0;
-        double max = -1.0E10;
-        double min = 1.0E10;
-        double stdev = 0.0;
+        double[] minmax = new double[2];
+        double term, mean, stdev;
         
         System.out.println("Enter 1 for keyboard input, 2 for file input: ");
         int mode = sc.nextInt();
@@ -22,7 +18,7 @@ public class JavaStats
         {
             System.out.println("When no more data is left to enter, simply hit return.");
             String s = "";
-            int sLen = 1;
+            int sLen;
 
             do
             {
@@ -33,37 +29,22 @@ public class JavaStats
                 if (sLen > 0)
                 {
                     term = Double.parseDouble(s);
-                    if (term > max)
-                    {
-                        max = term;
-                    }
-                    if (term < min)
-                    {
-                        min = term;
-                    }
-                    //dArr[n] = term;
                     dArrL.add(term);
                     n++;
-                    sum += term;
                 }
             } while (sLen > 0);
+            
             if (n > 0)
             {
-                mean = sum / (double) n;
+                minmax = ComputeExtremes(dArrL);
+                mean = ComputeMean(dArrL);
                 System.out.printf("For %d data point(s), \n", n);
-                System.out.printf("    the maximum is %.2f\n", max);
-                System.out.printf("    the minimum is %.2f\n", min);
+                System.out.printf("    the maximum is %.2f\n", minmax[1]);
+                System.out.printf("    the minimum is %.2f\n", minmax[0]);
                 System.out.printf("    the mean (average) is %.2f\n", mean);
                 if (n > 1)    // std dev is only defined if n > 1
                 {
-                    sum = 0.0;
-                    for (int i = 0; i < n; i++)
-                    {
-                        //term = dArr[i] - mean;
-                        term = dArrL.get(i) - mean;
-                        sum += term * term;
-                        stdev = Math.sqrt(sum / (double) (n - 1));
-                    }
+                    stdev = ComputeStdev(dArrL, mean);
                     System.out.printf("    the std dev is %.2f\n", stdev);
                 }
             }
@@ -80,6 +61,71 @@ public class JavaStats
             System.out.println("Invalid mode; exiting program.");
         }
     }
+    
+    private static double[] ComputeExtremes(ArrayList<Double> inArr)
+    {
+        double max = -1.0E10;
+        double min = 1.0E10;
+        double term;
+        double[] extremes = {0.0, 0.0};
+        int n = inArr.size();
+        if (n > 0)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                term = inArr.get(i);
+                if (term > max)
+                {
+                    max = term;
+                }
+                if (term < min)
+                {
+                    min = term;
+                }
+            }
+            extremes[0] = min;
+            extremes[1] = max;
+        }
+        
+        return extremes;
+    }
+    
+    private static double ComputeMean(ArrayList<Double> inArr)
+    {
+        double mean = 0.0;
+        int n = inArr.size();
+        if (n > 0)
+        {
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += inArr.get(i);
+            }
+            mean = sum / (double) n;
+        }
+        
+        return mean;
+    }
+
+    private static double ComputeStdev(ArrayList<Double> inArr, double mean)
+    {
+        double stdev = 0.0;
+        int n = inArr.size();
+        if (n > 1)
+        {
+            double sum = 0.0;
+            double term = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                term = inArr.get(i) - mean;
+                sum += term * term;
+            }
+            stdev = Math.sqrt(sum / (double) (n - 1));
+        }
+
+        return stdev;
+    }
+    
 }
 
 /*
@@ -94,7 +140,7 @@ Data point 4: 4
 Data point 5: 5
 Data point 6: 6
 Data point 7: 
-For 6 data points, 
+For 6 data point(s), 
     the maximum is 6.00
     the minimum is 1.00
     the mean (average) is 3.50
@@ -106,12 +152,12 @@ For 6 data points,
 Enter 1 for keyboard input, 2 for file input: 
 1
 When no more data is left to enter, simply hit return.
-Data point 1: 2
+Data point 1: 3.5
 Data point 2: 
-For 1 data points, 
-    the maximum is 2.00
-    the minimum is 2.00
-    the mean (average) is 2.00
+For 1 data point(s), 
+    the maximum is 3.50
+    the minimum is 3.50
+    the mean (average) is 3.50
 
  ----jGRASP: operation complete.
 
@@ -122,6 +168,4 @@ When no more data is left to enter, simply hit return.
 Data point 1: 
 No data was entered.
 
- ----jGRASP: operation complete.
-
-*/
+ ----jGRASP: operation complete.*/
