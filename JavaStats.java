6f48eb747a09f73cf51d1822a94dabf0716f9481
ekawamoto1 <eric.kawamoto@baxter-academy.org>
2020-06-25 11:21:26 -0400

    use array to persist entries for computing stdev

--- JavaStats.java
+++ JavaStats.java
@@ -5,15 +5,20 @@ public class JavaStats
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {
+        int N = 100;
+        double dArr[];
+        dArr = new double[N]; // initialize array with N elements
+        
         System.out.println("Enter 1 for keyboard input, 2 for file input: ");
         int mode = sc.nextInt();
-        sc.nextLine();
-        if (mode == 1)
+        sc.nextLine();  // this consumes the '\n' that follows the integer from the preceding entry
+        
+        if (mode == 1)    // data entered from keyboard
         {
             System.out.println("When no more data is left to enter, simply hit return.");
             String s = "";
             int sLen = 1;
-            double n = 0.0;
+            int n = 0;
             double term = 0.0;
             double sum = 0.0;
             double mean = 0.0;
@@ -22,13 +27,12 @@ public class JavaStats
             double stdev = 0.0;
             do
             {
-                System.out.printf("Data point %.0f: ", (n + 1));
+                System.out.printf("Data point %d: ", (n + 1));
                 s = sc.nextLine();
                 sLen = s.length();
                 //System.out.println("You entered " + s + ", length " + sLen);
                 if (sLen > 0)
                 {
-                    n++;
                     term = Double.parseDouble(s);
                     if (term > max)
                     {
@@ -38,23 +42,36 @@ public class JavaStats
                     {
                         min = term;
                     }
+                    dArr[n] = term;
+                    n++;
                     sum += term;
                 }
             } while (sLen > 0);
             if (n > 0)
             {
-                mean = sum / n;
-                System.out.printf("For %.0f data points, \n", n);
-                System.out.printf("The mean (average) is %.2f\n", mean);
-                System.out.printf("The maximum is %.2f\n", max);
-                System.out.printf("The minimum is %.2f\n", min);
+                mean = sum / (double) n;
+                System.out.printf("For %d data point(s), \n", n);
+                System.out.printf("    the maximum is %.2f\n", max);
+                System.out.printf("    the minimum is %.2f\n", min);
+                System.out.printf("    the mean (average) is %.2f\n", mean);
+                if (n > 1)    // std dev is only defined if n > 1
+                {
+                    sum = 0.0;
+                    for (int i = 0; i < n; i++)
+                    {
+                        term = dArr[i] - mean;
+                        sum += term * term;
+                        stdev = Math.sqrt(sum / (double) (n - 1));
+                    }
+                    System.out.printf("    the std dev is %.2f\n", stdev);
+                }
             }
             else
             {
                 System.out.println("No data was entered.");
             }
         }
-        else if (mode == 2)
+        else if (mode == 2)    // data read from file
         {
         }
         else
@@ -69,14 +86,40 @@ public class JavaStats
 Enter 1 for keyboard input, 2 for file input: 
 1
 When no more data is left to enter, simply hit return.
-Data point 1: 32
-Data point 2: 33
-Data point 3: 34
-Data point 4: 
-For 3 data points, 
-The mean (average) is 33.00
-The maximum is 34.00
-The minimum is 32.00
+Data point 1: 1
+Data point 2: 2
+Data point 3: 3
+Data point 4: 4
+Data point 5: 5
+Data point 6: 6
+Data point 7: 
+For 6 data points, 
+    the maximum is 6.00
+    the minimum is 1.00
+    the mean (average) is 3.50
+    the std dev is 1.87
+
+ ----jGRASP: operation complete.
+
+ ----jGRASP exec: java JavaStats
+Enter 1 for keyboard input, 2 for file input: 
+1
+When no more data is left to enter, simply hit return.
+Data point 1: 2
+Data point 2: 
+For 1 data points, 
+    the maximum is 2.00
+    the minimum is 2.00
+    the mean (average) is 2.00
+
+ ----jGRASP: operation complete.
+
+ ----jGRASP exec: java JavaStats
+Enter 1 for keyboard input, 2 for file input: 
+1
+When no more data is left to enter, simply hit return.
+Data point 1: 
+No data was entered.
 
  ----jGRASP: operation complete.
 
