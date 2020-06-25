4c55d89ac73a60b9dcf15c7e2b6cc5b79263d6f4
ekawamoto1 <eric.kawamoto@baxter-academy.org>
2020-06-24 23:25:25 -0400

    first change to source code

--- JavaStats.java
+++ JavaStats.java
@@ -1,8 +1,13 @@
+import java.util.*;
+
 public class JavaStats
 {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args)
     {
-        System.out.println("Hello, world!");
+        System.out.println("Enter 1 for keyboard input, 2 for file input: ");
+        int mode = sc.nextInt();
+        System.out.println("You entered " + mode);
     }
 }
 
